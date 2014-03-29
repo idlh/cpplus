8291e4ad28febd97b8a5c9732b646b71f30f1d6a
Camilo <>
2014-03-29 10:55:38 -0500

    Quitado de focusable a la tabla

--- src/Dialogos/HCDiag/Dprocedimientos.java
+++ src/Dialogos/HCDiag/Dprocedimientos.java
@@ -9,19 +9,30 @@ import Clases.Procedimientos;
 import HC.OrdenesM;
 import HC.ProcedimientosL;
 import java.awt.event.MouseAdapter;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Alvaro Monsalve
  */
 public class Dprocedimientos extends javax.swing.JDialog {
-    Clases.Procedimientos mos = new Clases.Procedimientos();
+    Clases.Procedimientos mos = new Clases.Procedimientos();    
+    public static DefaultTableModel modelo;
     //HC.ProcedimientosL pl = new ProcedimientosL();
     public Dprocedimientos(java.awt.Frame parent, boolean modal) {        
         super(parent, modal);
         initComponents();
-        jTable1.addMouseListener(new MouseAdapter() {});
-    } 
+        nombreloquesea();
+    }
+    
+    private void nombreloquesea(){    
+        getModelo();
+        jTable1.getTableHeader().setReorderingAllowed(false);
+        jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,1,3});
+        Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
+    }
+    
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -34,7 +45,7 @@ public class Dprocedimientos extends javax.swing.JDialog {
         jTextField1 = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jLabel2 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(470, 381));
@@ -72,7 +83,7 @@ public class Dprocedimientos extends javax.swing.JDialog {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 391, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(69, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -113,6 +124,7 @@ public class Dprocedimientos extends javax.swing.JDialog {
 
             }
         ));
+        jTable1.setFocusable(false);
         jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseReleased(java.awt.event.MouseEvent evt) {
                 jTable1MouseReleased(evt);
@@ -120,8 +132,14 @@ public class Dprocedimientos extends javax.swing.JDialog {
         });
         jScrollPane1.setViewportView(jTable1);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel2.setText("Doble click al procedimiento para añadirlo.");
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/sigA0.PNG"))); // NOI18N
+        jButton3.setContentAreaFilled(false);
+        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton3.setFocusable(false);
+        jButton3.setMaximumSize(new java.awt.Dimension(36, 36));
+        jButton3.setMinimumSize(new java.awt.Dimension(36, 36));
+        jButton3.setPreferredSize(new java.awt.Dimension(36, 36));
+        jButton3.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/sigA1.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -129,26 +147,26 @@ public class Dprocedimientos extends javax.swing.JDialog {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(10, 10, 10)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 450, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 10, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(6, 6, 6)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel2)
-                .addGap(17, 17, 17))
+                .addGap(6, 6, 6)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -166,7 +184,7 @@ public class Dprocedimientos extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyReleased
-        TableRowSorter sorter = new TableRowSorter(Clases.Procedimientos.modelo);
+        TableRowSorter sorter = new TableRowSorter(modelo);
         sorter.setRowFilter (RowFilter.regexFilter(jTextField1.getText().toUpperCase()));
         jTable1.setRowSorter (sorter);
     }//GEN-LAST:event_jTextField1KeyReleased
@@ -203,62 +221,66 @@ public class Dprocedimientos extends javax.swing.JDialog {
     }//GEN-LAST:event_jTable1MouseReleased
     public void procotros(){
         try {
-                mos.getModelo();
-                jTable1.getTableHeader().setReorderingAllowed(false);
-                jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,2});
-                Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
                 mos.Mostrar_datosO();            
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
     public void procquirur(){
         try {
-                mos.getModelo();
-                jTable1.getTableHeader().setReorderingAllowed(false);
-                jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,2});
-                Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
                 mos.Mostrar_datosQ();            
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
     public void procimage(){
         try {
-                mos.getModelo();
-                jTable1.getTableHeader().setReorderingAllowed(false);
-                jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,2});
-                Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
                 mos.Mostrar_datosI();
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
     public void proclab(){
         try {
-                mos.getModelo();
-                jTable1.getTableHeader().setReorderingAllowed(false);
-                jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,2});
-                Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
-                mos.Mostrar_datosL();
+            mos.Mostrar_datosL();
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
+                
     }
     public void procMon(){
         try {
-                mos.getModelo();
-                jTable1.getTableHeader().setReorderingAllowed(false);
-                jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                Funciones_AD.setOcultarColumnas(jTable1, new int[]{0,2});
-                Funciones_AD.setSizeColumnas(jTable1, new int[]{1}, new int[]{450});
                 mos.Mostrar_datosM();
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
+     public void getModelo(){
+         modelo = new DefaultTableModel(
+        null, new String [] {"Id","Codigo", "Procedimiento", "Categoria"}){
+            Class[] types = new Class []{
+                    java.lang.String.class,
+                    java.lang.String.class,
+                    java.lang.String.class,
+                    java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false,false,false,false
+            };
+            @Override
+            public Class getColumnClass(int columnIndex) {
+               return types [columnIndex];
+            }
+            @Override
+            public boolean isCellEditable(int rowIndex, int colIndex){
+               return canEdit [colIndex];
+            }
+        };
+        jTable1.setModel(modelo); 
+    }   
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
