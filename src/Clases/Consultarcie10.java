16822897e0497c241f4634133528858f26f06f44
Administrador <Administrador@Developer3>
2014-04-08 08:54:35 -0500

    Tabulacion de code

--- src/Clases/Consultarcie10.java
+++ src/Clases/Consultarcie10.java
@@ -5,22 +5,28 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Consultarcie10 {
+
     Clases.BDConectar db = new Clases.BDConectar();
+
     public void llamar_cie(String codigo){
         try {
             db.ConectarBasedeDatos();
-            db.resultado = db.sentencia.executeQuery("SELECT CONCAT(`codigo`,' - ', `descripcion`) AS cie FROM `database`.`static_cie10` WHERE (`codigo` ='"+codigo+"');");
-            if(db.resultado != null){
-                while(db.resultado.next()){
+            db.resultado = db.sentencia.executeQuery("SELECT CONCAT(`codigo`,' - ', `descripcion`) AS cie FROM `database`.`static_cie10` WHERE (`codigo` ='" + codigo + "');");
+            if (db.resultado != null) {
+                while (db.resultado.next()) {
                     HC.DiagnosticosM.jTextField1.setText(db.resultado.getString("cie"));
                     HC.DiagnosticosM.jTextField1.setToolTipText(db.resultado.getString("cie"));
                 }
-            }else{
+            } else {
                 JOptionPane.showMessageDialog(null, "Ningun dato encontrado", "MENSAJE DE INFORMACION", JOptionPane.PLAIN_MESSAGE);
             }
-        } catch (HeadlessException | SQLException e) {
+        } catch (HeadlessException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
-        }finally{
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
             db.DesconectarBasedeDatos();
         }
     }
