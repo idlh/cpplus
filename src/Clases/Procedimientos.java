e944365bed5f69620190caa0aaf80a5d4713249a
Administrador <Administrador@Developer3>
2014-04-21 07:15:01 -0500

    Consultas de procedimientos referentes a planificacion familiar

--- src/Clases/Procedimientos.java
+++ src/Clases/Procedimientos.java
@@ -45,15 +45,15 @@ public class Procedimientos {
             bd.ConectarBasedeDatos();
             i = 0;
             Object s[] = null;
-            bd.resultado = bd.sentencia.executeQuery("SELECT\n"
-                    + "      `config_cups`.`id`\n"
-                    + "    , `config_cups`.`codigo`\n"
-                    + "    , `config_cups`.`de_subcategoria`\n"
-                    + "    , `static_estructura_cups`.`des_capitulo`\n"
+            bd.resultado = bd.sentencia.executeQuery("SELECT"
+                    + "      `config_cups`.`id`"
+                    + "    , `config_cups`.`codigo`"
+                    + "    , `config_cups`.`de_subcategoria`"
+                    + "    , `static_estructura_cups`.`des_capitulo`"
                     + "FROM\n"
-                    + "    `database`.`config_cups`\n"
-                    + "    INNER JOIN `database`.`static_estructura_cups`\n"
-                    + "       ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)\n"
+                    + "    `database`.`config_cups`"
+                    + "    INNER JOIN `database`.`static_estructura_cups`"
+                    + "       ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
                     + "       where(`config_cups`.`estado_urg`<>'0' and `static_estructura_cups`.`id`<15);");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
@@ -151,6 +151,38 @@ public class Procedimientos {
                     + "    INNER JOIN `database`.`static_estructura_cups`\n"
                     + "       ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)\n"
                     + "       WHERE(`config_cups`.`estado_urg`<>'0' AND `static_estructura_cups`.`id`=16);");
+            if (bd.resultado != null) {
+                while (bd.resultado.next()) {
+                    Dialogos.HCDiag.Dprocedimientos.modelo.addRow(s);
+                    Dialogos.HCDiag.Dprocedimientos.modelo.setValueAt("" + bd.resultado.getString("id"), i, 0);
+                    Dialogos.HCDiag.Dprocedimientos.modelo.setValueAt("" + bd.resultado.getString("codigo"), i, 1);
+                    Dialogos.HCDiag.Dprocedimientos.modelo.setValueAt("" + bd.resultado.getString("de_subcategoria"), i, 2);
+                    Dialogos.HCDiag.Dprocedimientos.modelo.setValueAt("" + bd.resultado.getString("des_capitulo"), i, 3);
+                    i++;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage().toString());
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage().toString());
+        }
+    }
+    
+    public void Mostrar_datosanti() {
+        try {
+            bd.ConectarBasedeDatos();
+            i = 0;
+            Object s[] = null;
+            bd.resultado = bd.sentencia.executeQuery("SELECT"
+                    + "      `config_cups`.`id`"
+                    + "    , `config_cups`.`codigo`"
+                    + "    , `config_cups`.`de_subcategoria`"
+                    + "    , `static_estructura_cups`.`des_capitulo`"
+                    + "FROM"
+                    + "    `database`.`config_cups`"
+                    + "    INNER JOIN `database`.`static_estructura_cups`"
+                    + "       ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
+                    + "       where(`config_cups`.`estado_urg`<>'0') AND ((`config_cups`.`codigo` = '663100') OR (`config_cups`.`codigo` = '697100') OR (`config_cups`.`codigo` = '861203') OR (`config_cups`.`codigo` = '677101') OR (`config_cups`.`codigo` = '637300'));");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
                     Dialogos.HCDiag.Dprocedimientos.modelo.addRow(s);
