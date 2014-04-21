2e67ae868012e1ef9166256628789faeb2b50aad
Administrador <Administrador@Developer3>
2014-04-21 07:13:02 -0500

    Consultas referentes a los programas del parto y postparto

--- src/Clases/CargarordenesM.java
+++ src/Clases/CargarordenesM.java
@@ -35,7 +35,7 @@ public class CargarordenesM {
                     + "    INNER JOIN `database`.`static_estructura_cups` "
                     + "        ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
                     + " WHERE (`pyp_procedimiento`.`id_historiapyp` = '" + id + "'"
-                    + "    AND `static_estructura_cups`.`id` ='" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2);");
+                    + "    AND `static_estructura_cups`.`id` ='" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2 AND `pyp_procedimiento`.`tipo` = 0);");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
                     modelo.addRow(s);
@@ -76,7 +76,7 @@ public class CargarordenesM {
                     + "    INNER JOIN `database`.`static_estructura_cups` "
                     + "        ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
                     + " WHERE (`pyp_procedimiento`.`id_historiapyp` = '" + id + "'"
-                    + "    AND `static_estructura_cups`.`id`<'" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2);");
+                    + "    AND `static_estructura_cups`.`id`<'" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2 AND `pyp_procedimiento`.`tipo` = 0);");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
                     modelo.addRow(s);
@@ -117,7 +117,7 @@ public class CargarordenesM {
                     + "    INNER JOIN `database`.`static_estructura_cups` "
                     + "        ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
                     + " WHERE (`pyp_procedimiento`.`id_historiapyp` = '" + id + "'"
-                    + "    AND `static_estructura_cups`.`id`>'" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2);");
+                    + "    AND `static_estructura_cups`.`id`>'" + idestructura + "' AND `pyp_procedimiento`.`estado` = 2 AND `pyp_procedimiento`.`tipo` = 0);");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
                     modelo.addRow(s);
@@ -157,7 +157,7 @@ public class CargarordenesM {
                     + "    INNER JOIN `database`.`sum_suministro` "
                     + "        ON (`pyp_posologia`.`id_suministro` = `sum_suministro`.`id`)"
                     + " WHERE (`pyp_posologia`.`id_historiac` ='" + id + "'"
-                    + "    AND `pyp_posologia`.`estado` =2);");
+                    + "    AND `pyp_posologia`.`estado` =2 AND `pyp_posologia`.`tipo` = 0);");
             if (bd.resultado != null) {
                 while (bd.resultado.next()) {
                     modelo.addRow(s);
@@ -212,6 +212,90 @@ public class CargarordenesM {
             JOptionPane.showMessageDialog(null, "tab004 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "tab004 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void cargartablamedianti(DefaultTableModel modelo, String id) {
+        try {
+            bd.ConectarBasedeDatos();
+            c = 0;
+            Object s[] = null;
+            bd.resultado = bd.sentencia.executeQuery("SELECT"
+                    + "    `pyp_posologia`.`id_suministro`"
+                    + "    , `sum_suministro`.`suministro`"
+                    + "    , `pyp_posologia`.`dosis_n`"
+                    + "    , `pyp_posologia`.`dosis_u`"
+                    + "    , `pyp_posologia`.`via`"
+                    + "    , `pyp_posologia`.`administracion`"
+                    + "    , `pyp_posologia`.`cantidad`"
+                    + "    , `pyp_posologia`.`estado`"
+                    + " FROM"
+                    + "    `database`.`pyp_posologia`"
+                    + "    INNER JOIN `database`.`sum_suministro` "
+                    + "        ON (`pyp_posologia`.`id_suministro` = `sum_suministro`.`id`)"
+                    + " WHERE (`pyp_posologia`.`id_historiac` ='" + id + "'"
+                    + "    AND `pyp_posologia`.`estado` = 2 AND `pyp_posologia`.`tipo` = 1);");
+            if (bd.resultado != null) {
+                while (bd.resultado.next()) {
+                    modelo.addRow(s);
+                    modelo.setValueAt(bd.resultado.getString("id_suministro"), c, 0);
+                    modelo.setValueAt(bd.resultado.getString("suministro"), c, 1);
+                    modelo.setValueAt(bd.resultado.getString("dosis_n"), c, 2);
+                    modelo.setValueAt(bd.resultado.getString("dosis_u"), c, 3);
+                    modelo.setValueAt(bd.resultado.getString("via"), c, 4);
+                    modelo.setValueAt(bd.resultado.getString("administracion"), c, 5);
+                    modelo.setValueAt(bd.resultado.getString("cantidad"), c, 6);
+                    modelo.setValueAt(bd.resultado.getString("estado"), c, 7);
+                    c++;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "tab004 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "tab004 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+        } finally {
+            bd.DesconectarBasedeDatos();
+        }
+    }
+    
+    public void cargartablaanti(DefaultTableModel modelo, String id) {
+        try {
+            bd.ConectarBasedeDatos();
+            c = 0;
+            Object s[] = null;
+            bd.resultado = bd.sentencia.executeQuery("SELECT"
+                    + "    `pyp_procedimiento`.`id_cups` AS `id`"
+                    + "    , `config_cups`.`codigo`"
+                    + "    , `config_cups`.`de_subcategoria` AS `procedimiento`"
+                    + "    , `static_estructura_cups`.`des_capitulo` AS `categoria`"
+                    + "    , `pyp_procedimiento`.`estado`"
+                    + " FROM"
+                    + "    `database`.`pyp_procedimiento`"
+                    + "    INNER JOIN `database`.`pyp_historiac` "
+                    + "        ON (`pyp_procedimiento`.`id_historiapyp` = `pyp_historiac`.`id`)"
+                    + "    INNER JOIN `database`.`config_cups` "
+                    + "        ON (`pyp_procedimiento`.`id_cups` = `config_cups`.`id`)"
+                    + "    INNER JOIN `database`.`static_estructura_cups` "
+                    + "        ON (`config_cups`.`id_estructura_cups` = `static_estructura_cups`.`id`)"
+                    + " WHERE (`pyp_procedimiento`.`id_historiapyp` = '" + id + "'"
+                    + "    AND `pyp_procedimiento`.`tipo`= 1 AND `pyp_procedimiento`.`estado` = 2);");
+            if (bd.resultado != null) {
+                while (bd.resultado.next()) {
+                    modelo.addRow(s);
+                    modelo.setValueAt(bd.resultado.getString("id"), c, 0);
+                    modelo.setValueAt(bd.resultado.getString("codigo"), c, 1);
+                    modelo.setValueAt(bd.resultado.getString("procedimiento"), c, 2);
+                    modelo.setValueAt(bd.resultado.getString("categoria"), c, 3);
+                    modelo.setValueAt(bd.resultado.getString("estado"), c, 4);
+                    c++;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "tab005 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "tab005 " + e.getMessage().toString(), CargarordenesM.class.getName(), JOptionPane.INFORMATION_MESSAGE);
+        } finally {
+            bd.DesconectarBasedeDatos();
         }
     }
 }
