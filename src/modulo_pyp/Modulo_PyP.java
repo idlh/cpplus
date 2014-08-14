06b50a7dac3930c775d58c3522dd7651fb382e97
Camilo <>
2014-08-14 14:26:49 -0500

    14/08/2014

--- src/modulo_pyp/Modulo_PyP.java
+++ src/modulo_pyp/Modulo_PyP.java
@@ -11,12 +11,12 @@ import javax.swing.UIManager;
  * @author IdlhDeveloper
  */
 public class Modulo_PyP {
-public static Desktop d ;
+public static Desktop d;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
          try {
          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         }catch(Exception e){
